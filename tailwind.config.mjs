b4d4d5d82cdb3cdcb9db5a02f62cf8ab71d94c2b
Isahir Zavala Miranda <isahirzavala@hotmail.com>
2024-12-17 13:21:40 -0600

    fix: update styling in Experience and Proyects components, add text gradient animation in Tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,31 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		extend: {
+		  animation: {
+			'text-gradient': 'text-gradient 1.5s linear infinite'
+		  },
+		  keyframes: {
+			'text-gradient': {
+			  'to': {
+				backgroundPosition: '200% center'
+			  }
+			}
+		  }
+		}
+	  },
+	plugins: []
 }
+
+/**
+ * "animation": {
+    "text-gradient": "text-gradient 1.5s linear infinite"
+  },
+  "keyframes": {
+    "text-gradient": {
+      "to": {
+        "backgroundPosition": "200% center"
+      }
+    }
+  }
+ */
